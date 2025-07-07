b8a1127da35b99c32baf5182c9ef909636f1a680
tayler20166 <tayler20166@gmail.com>
2025-07-07 12:33:45 -0500

    feat: enhance item fetching by combining projects from DB and projects from Git API

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,8 +29,20 @@
 	}
 
 	async function fetchItems(): Promise<void> {
-		const res = await fetch('/api/items');
-		items = await res.json();
+		const res1 = await fetch('/api/items');
+		const res2 = await fetch(import.meta.env.VITE_GITHUB_API_URL);
+
+		const DBData = await res1.json();
+		const githubData = await res2.json();
+		const transformedGithubData = githubData.map((repo) => ({
+			id: repo.id,
+			title: repo.name,
+			description: repo.description || 'No description provided.',
+			img_url: null,
+			url: repo.html_url
+		}));
+
+		items = [...DBData, ...transformedGithubData];
 	}
 	let pageLoaded = false;
 	onMount(async () => {
@@ -75,14 +87,20 @@
 
 				<div class="relative container px-4 mx-auto text-[var(--color-contrast)] py-6 pb-32">
 					<div class="sm:flex text-center">
-						<div class="sm:flex-1 text-2xl font-bold sm:text-left mb-4">
-							<a href="/" class=" start-anim scroll-anim hover:underline">Artem Kamkov</a>
+						<div class="start-anim scroll-anim sm:flex-1 text-2xl font-bold sm:text-left mb-4">
+							<a href="/" class="hover:opacity-70 transition">Artem Kamkov</a>
 						</div>
 						<div class="start-anim scroll-anim sm:flex-2 sm:text-right">
 							<ul class="text-2xl">
 								<li class="inline-block mr-6"><NightToggle /></li>
 								<li class="inline-block mr-6">
-									<a href={gitUrl} target="_blank" rel="noopener" aria-label="Github">
+									<a
+										href={gitUrl}
+										class="hover:opacity-70 transition"
+										target="_blank"
+										rel="noopener"
+										aria-label="Github"
+									>
 										<svg
 											class="enlarge w-6 h-6"
 											fill="currentColor"
@@ -95,7 +113,13 @@
 									</a>
 								</li>
 								<li class="inline-block">
-									<a href={LinkedinUrl} target="_blank" rel="noopener" aria-label="LinkedIn">
+									<a
+										href={LinkedinUrl}
+										class="hover:opacity-70 transition"
+										target="_blank"
+										rel="noopener"
+										aria-label="LinkedIn"
+									>
 										<svg
 											class="enlarge w-6 h-6"
 											fill="currentColor"
@@ -137,10 +161,7 @@
 				viewBox="0 0 100 100"
 				preserveAspectRatio="none"
 			>
-				<polygon
-					class="fill-[var(--color-bg-primary)] dark:fill-[var(--color-bg-secondary)]"
-					points="0,100 100,0 100,100"
-				></polygon>
+				<polygon class="fill-[var(--color-bg-secondary)]" points="0,100 100,0 100,100"></polygon>
 			</svg>
 		</div>
 
@@ -152,16 +173,33 @@
 					>
 						Projects
 					</h2>
-					<div class="md:flex mt-12 mb-12 md:mb-16">
+					<div class="flex flex-wrap gap-4 mt-12 mb-12 md:mb-16">
 						{#each items as item}
-							<div class="scroll-anim project sm:flex-1 px-4 lg:px-8 mb-8 md:mb-0">
+							<div
+								class="scroll-anim project w-full md:w-[calc(50%-0.5rem)] px-4 lg:px-8 mb-8 md:mb-0"
+							>
 								<a href={item.url} target="_blank" rel="noopener">
 									<div
 										class="bg-[var(--color-bg-primary)] enlarge text-center text-base rounded-lg border-t-6 border-primary shadow-lg h-full px-8 md:px-12 py-8"
 									>
 										<div class="flex justify-center mt-5 mb-6">
 											<div class="flex items-center h-24 dark:bg-[var(--color-bg-primary)]/20 p-3">
-												<img src={item.img_url} class="max-h-full" alt="" />
+												{#if item.img_url}
+													<img
+														src={item.img_url}
+														class="min-w-[80px] min-h-[80px] max-h-full h-auto w-auto object-contain"
+														alt=""
+													/>
+												{:else}
+													<svg
+														class="min-w-[80px] min-h-[80px] max-h-full h-auto w-auto object-contain fill-[var(--color-primary)]"
+														xmlns="http://www.w3.org/2000/svg"
+														viewBox="0 0 496 512"
+														><path
+															d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+														></path></svg
+													>
+												{/if}
 											</div>
 										</div>
 										<p
@@ -260,7 +298,13 @@
 					<div class="sm:flex-1 sm:text-right sm:order-2 mb-1 sm:mb-0">
 						<ul class="text-2xl">
 							<li class="inline-block mr-6">
-								<a href={gitUrl} target="_blank" rel="noopener" aria-label="Github">
+								<a
+									href={gitUrl}
+									class="hover:opacity-70 transition"
+									target="_blank"
+									rel="noopener"
+									aria-label="Github"
+								>
 									<svg
 										class="enlarge w-6 h-6"
 										fill="currentColor"
@@ -273,7 +317,13 @@
 								</a>
 							</li>
 							<li class="inline-block">
-								<a href={LinkedinUrl} target="_blank" rel="noopener" aria-label="LinkedIn">
+								<a
+									href={LinkedinUrl}
+									class="hover:opacity-70 transition"
+									target="_blank"
+									rel="noopener"
+									aria-label="LinkedIn"
+								>
 									<svg
 										class="enlarge w-6 h-6"
 										fill="currentColor"
